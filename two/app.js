06eba9f8ccf00efad07a3721c7bc36ce97d8d151
sanskar <sanskar.003.02@gmail.com>
2025-09-10 17:04:25 +0530

    modified for deployment

--- two/app.js
+++ two/app.js
@@ -2,11 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import connectDB from "../config/db.js";
-import clothRoutes from "../routes/cloths.routes.js";
-import authRoutes from "../routes/auth.routes.js";
-import personalInfoRoutes from "../routes/personalInfo.routes.js";
-import searchRoutes from "../routes/search.routes.js";
+import connectDB from "./config/db.js";
+import clothRoutes from "./routes/cloths.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import personalInfoRoutes from "./routes/personalInfo.routes.js";
+import searchRoutes from "./routes/search.routes.js";
 
 dotenv.config();
 const app = express();
