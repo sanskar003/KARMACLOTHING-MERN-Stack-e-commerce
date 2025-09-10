dc17a497bcf985e6cf725282d1639a31352ababb
sanskar <sanskar.003.02@gmail.com>
2025-09-10 22:05:05 +0530

    modified for deployment

--- two/config/db.js
+++ two/config/db.js
@@ -3,16 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+let isConnected;
+
 const connectDB = async () => {
-    try {
-        const uri = process.env.MONGO_URI;
-        if (!uri) throw new Error("MONGO_URI is undefined! Check your .env file.");
-        
-        await mongoose.connect(uri);
-        console.log("MongoDB Connected Successfully!");
-    } catch (error) {
-        console.error("MongoDB Connection Failed:", error);
-    }
+  if (isConnected) {
+    console.log("♻️ Using existing MongoDB connection");
+    return;
+  }
+
+  try {
+    const uri = process.env.MONGO_URI;
+    if (!uri) throw new Error("MONGO_URI is undefined! Check your .env file.");
+
+    const db = await mongoose.connect(uri);
+    isConnected = db.connections[0].readyState;
+    console.log("✅ MongoDB Connected Successfully!");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed:", error);
+  }
 };
 
 export default connectDB;
